91bd49e0ac6b8a061124a53fe846032674453453
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-04 21:33:59 +0530

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HPRight from '../../assets/HomePageRight.png'
+import { BsDot } from 'react-icons/bs'
 
 const HomePage = () => {
   return (
@@ -41,9 +42,36 @@ const HomePage = () => {
 
             <div className="md:flex my-8">
                 <div className="md:my-0 my-4 w-full md:mr-4 bg-white py-4 px-6 duration-500 hover:shadow-2xl">
-                    <h1 className="text-purple-600 text-xl font-semibold text-center">Our Story</h1>
+                    <h1 className="text-purple-600 text-xl font-semibold text-center">Our Values</h1>
+                    <div className="">
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Innovation:</span> We are constantly exploring new technologies and methodologies to stay ahead of the curve.</p>
+                        </p>
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Integrity:</span> We believe in honesty, transparency, and building long-lasting relationships based on trust.</p>
+                        </p>
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Customer-Centricity:</span> Our clients are at the heart of everything we do. We are committed to understanding their needs and exceeding their expectations.</p>
+                        </p>
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Excellence:</span> We take pride in delivering high-quality solutions that drive real results.</p>
+                        </p>
+                    </div>
                 </div>
-                <div className="md:my-0 my-4 w-full md:mr-4 bg-white py-4 px-6 duration-500 hover:shadow-2xl"></div>
+                <div className="md:my-0 my-4 w-full md:mr-4 bg-white py-4 px-6 duration-500 hover:shadow-2xl">
+                    <h1 className="text-purple-600 text-xl font-semibold text-center">Why Choose Us?</h1>
+                    <div className="">
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Expert Team:</span> Our team of skilled developers, designers, and strategists bring a wealth of knowledge and experience to every project.</p>
+                        </p>
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Tailored Solutions:</span> We understand that no two businesses are the same. That’s why we offer customized solutions that align with your goals.</p>
+                        </p>
+                        <p className="flex py-2">
+                            <p className="mt-1"><span className='text-purple-600 font-semibold'>Proven Track Record:</span> We have successfully delivered numerous projects that have helped our clients achieve their business objectives.</p>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
