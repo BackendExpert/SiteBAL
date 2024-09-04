770c12f50bae78f4d91c8d62bf3485dea0bc368a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-04 20:15:50 +0530

    Update NavBar.jsx

--- src/components/HomePage/NavBar.jsx
+++ src/components/HomePage/NavBar.jsx
@@ -1,44 +1,62 @@
-import React from 'react'
-import Logo from '../../assets/Logo.png'
-import { BsList } from 'react-icons/bs'
+import React, { useState } from 'react';
+import Logo from '../../assets/Logo.png';
+import { BsList } from 'react-icons/bs';
 
 const NavBar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     const navData = [
-        {id: 1, name: 'Home', link: ''},
-        {id: 2, name: 'About Us', link: ''},
-        {id: 3, name: 'Services', link: ''},
-        {id: 4, name: 'Portfolio', link: ''},
-        {id: 5, name: 'Blog', link: ''},
-        {id: 6, name: 'Careers', link: ''},
-        {id: 7, name: 'Contact Us', link: ''},
-    ]
-  return (
-    <div className='h-auto py-4 px-6 bg-white  shadow-md text-purple-900'>
-        <div className="md:flex justify-between ml-4">
-            <div className="">
-                <h1 className="">BLACK ALPHA LABS</h1>
+        { id: 1, name: 'Home', link: '' },
+        { id: 2, name: 'About Us', link: '' },
+        { id: 3, name: 'Services', link: '' },
+        { id: 4, name: 'Portfolio', link: '' },
+        { id: 5, name: 'Blog', link: '' },
+        { id: 6, name: 'Careers', link: '' },
+        { id: 7, name: 'Contact Us', link: '' },
+    ];
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <div className='h-auto py-4 px-6 bg-white shadow-md text-purple-900'>
+            <div className="flex justify-between items-center">
+                <div className="">
+                    <h1 className="text-lg font-bold">BLACK ALPHA LABS</h1>
+                </div>
+
+                <div className="md:hidden">
+                    <button onClick={toggleMobileMenu}>
+                        <BsList size={24} />
+                    </button>
+                </div>
+
+                <div className="hidden md:flex space-x-6">
+                    {navData.map((data) => (
+                        <a href={data.link} key={data.id} className="block duration-500 hover:underline">
+                            {data.name}
+                        </a>
+                    ))}
+                </div>
             </div>
 
-            <div className="md:hiddem block">
-                <BsList />
-            </div>
-
-            <div className="md:flex md:block hidden">
-                {
-                    navData.map((data, index) => {
-                        return (
-                            <div className="mx-4" key={index}>
-                                <a href={data.link}>
-                                    {data.name}
-                                </a>
-                            </div>
-                        )
-                    })
-                }
+            {/* Mobile Menu */}
+            <div
+                className={`absolute top-0 left-0 w-full bg-white shadow-md transition-transform duration-300 ease-in-out transform ${
+                    isMobileMenuOpen ? 'translate-y-14' : '-translate-y-full'
+                } md:hidden`}
+            >
+                <div className="py-4">
+                    {navData.map((data) => (
+                        <a href={data.link} key={data.id} className="block py-2 px-4 border-b border-gray-200">
+                            {data.name}
+                        </a>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
