2168e6ddfd6ff3227aa0a9d856a764a0626dd9cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-04 20:36:13 +0530

    Update

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='min-h-screen bg-purple-300/10 pt-28 px-6'>
-        lorem*400
+        <div className="md:flex justify-between">
+            <div className="">
+                <p className="">Unleashing the Power of Technology for <br /> Your Success with Black alpha labs</p>
+            </div>
+            <div className="">
+                hi all
+            </div>
+        </div>
     </div>
   )
 }
