ed9f22dbf626a69c7b9bdfdf55ee47ca3ad99240
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-05 07:57:12 +0530

    Update OurPortfolio.jsx

--- src/components/HomePage/OurPortfolio.jsx
+++ src/components/HomePage/OurPortfolio.jsx
@@ -53,7 +53,7 @@ const OurPortfolio = () => {
                 {
                     Projects.map((project, index) => {
                         return (
-                            <div className="bg-white p-4 rounded duration-500 hover:shadow-xl" key={index}>
+                            <div className="bg-white p-4 rounded duration-500 hover:shadow-xl md:my-0 my-4" key={index}>
                                 <center><img src={project.img} alt="" className='h-40 w-auto'/></center>
                                 <h1 className="text-center text-purple-500 font-bold">{project.name}</h1>
                             </div>
