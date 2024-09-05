ba9aec04484af073274af91f3451245a2d9dc468
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-05 15:57:30 +0530

    Update

--- src/components/HomePage/Tech.jsx
+++ src/components/HomePage/Tech.jsx
@@ -1,139 +1,22 @@
 import React from 'react'
 
 const Tech = () => {
-    const TechStack = [
-        {
-            id: 1,
-            name: 'Frontend Technologies',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        },
-
-        {
-            id: 2,
-            name: 'Backend Technologies',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        },
-        {
-            id: 3,
-            name: 'Database Technologies',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        },
-        {
-            id: 4,
-            name: 'Cloud Platforms',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        },
-        {
-            id: 5,
-            name: 'DevOps Tools',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        },
-        {
-            id: 7,
-            name: 'Testing & Monitoring Tools',
-            technology: [
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-                {
-                    id: 1,
-                    name: 'React',
-                    logo: ''
-                },
-            ]
-        }
-    ]
   return (
     <div data-aos="zoom-in" className="w-full mx-auto pt-16">
-      <h2 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 mb-12">Our Technology Stack
-      </h2>
+      <h2 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 mb-12">Our Technology Stack</h2>
+    
+        <div className="">
+            <div className="grid md:grid-cols-8 grid-cols-4 gap-4">
+                <div className="p-2">
+                    <img src="https://img.icons8.com/?size=48&id=20909&format=png" alt="" />
+                </div>
+                <div className="p-2">hi all</div>
+                <div className="p-2">hi all</div>
+                <div className="p-2">hi all</div>
+                <div className="p-2">hi all</div>
+                <div className="p-2">hi all</div>
+            </div>
+        </div>
     </div>
   )
 }
