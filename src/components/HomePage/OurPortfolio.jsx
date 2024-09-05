b23d6a25fadd451b7adbfbf16fcfa703f760c944
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-05 07:55:03 +0530

    Update OurPortfolio.jsx

--- src/components/HomePage/OurPortfolio.jsx
+++ src/components/HomePage/OurPortfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ServiceImg from '../../assets/Services.png'
 
 const OurPortfolio = () => {
-    const Project = [
+    const Projects = [
         {
             id: 1, 
             name: 'Smart Retail E-Commerce Platform', 
@@ -46,17 +46,25 @@ const OurPortfolio = () => {
     ]
   return (
     <div className="pt-16">
-        <h1 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 ">Our Services</h1>
-
-        <div className="md:grid grid-cols-3 gap-4 my-4">
-
-        </div>
+        <h1 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 ">Our Portfolio</h1>
 
         <div className="">
+            <div className="md:grid grid-cols-4 gap-4 mt-16">
+                {
+                    Projects.map((project, index) => {
+                        return (
+                            <div className="bg-white p-4 rounded duration-500 hover:shadow-xl" key={index}>
+                                <center><img src={project.img} alt="" className='h-40 w-auto'/></center>
+                                <h1 className="text-center text-purple-500 font-bold">{project.name}</h1>
+                            </div>
+                        )
+                    })
+                }
+            </div>
             <center>
                 <a href="">
                     <div className="mt-8">
-                        <button className='bg-purple-600 text-white py-4 px-16 rounded-full duration-500 hover:bg-purple-700'>Get Started</button>
+                        <button className='bg-purple-600 text-white py-4 px-16 rounded-full duration-500 hover:bg-purple-700'>View Our Projects</button>
                     </div>
                 </a>
             </center>
