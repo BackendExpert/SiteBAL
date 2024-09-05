a3758de6097f85f26ea7540b231b394e8149b293
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-05 15:17:54 +0530

    Update Tech.jsx

--- src/components/HomePage/Tech.jsx
+++ src/components/HomePage/Tech.jsx
@@ -1,8 +1,140 @@
 import React from 'react'
 
 const Tech = () => {
+    const TechStack = [
+        {
+            id: 1,
+            name: 'Frontend Technologies',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        },
+
+        {
+            id: 2,
+            name: 'Backend Technologies',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        },
+        {
+            id: 3,
+            name: 'Database Technologies',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        },
+        {
+            id: 4,
+            name: 'Cloud Platforms',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        },
+        {
+            id: 5,
+            name: 'DevOps Tools',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        },
+        {
+            id: 7,
+            name: 'Testing & Monitoring Tools',
+            technology: [
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+                {
+                    id: 1,
+                    name: 'React',
+                    logo: ''
+                },
+            ]
+        }
+    ]
   return (
-    <div>Tech</div>
+    <div data-aos="zoom-in" className="w-full mx-auto pt-16">
+      <h2 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 mb-12">Our Technology Stack
+      </h2>
+    </div>
   )
 }
 
