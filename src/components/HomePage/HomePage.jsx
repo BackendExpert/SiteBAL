3f0f2e7145fc1c19bb342d91a582f0b9a196b936
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-05 07:36:26 +0530

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import HPRight from '../../assets/HomePageRight.png'
 import { BsDot } from 'react-icons/bs'
+import ServiceImg from '../../assets/Services.png'
 
 const HomePage = () => {
+    const services = [
+        {id: 1, name: 'Custom Software Development', img: ServiceImg},
+        {id: 2, name: 'Web Development', img: HPRight},
+        {id: 3, name: 'IT Consulting', img: ServiceImg},
+        {id: 4, name: 'Cloud Solutions', img: HPRight},
+        {id: 5, name: 'UI/UX Design', img: ServiceImg},
+        {id: 6, name: 'IT Support and Maintenance', img: HPRight},        
+    ]
+
   return (
     <div className='min-h-screen bg-purple-300/10 pt-20 px-6'>
         <div className="md:flex justify-between">
@@ -77,6 +87,29 @@ const HomePage = () => {
 
         <div className="pb-8 md:px-8">
             <h1 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 ">Our Services</h1>
+
+            <div className="md:grid grid-cols-3 gap-4 my-4">
+                {
+                    services.map((service, index) => {
+                        return (
+                            <div key={index} className="w-full py-4 px-6 bg-white duration-500 rounded-xl hover:shadow-xl mb-8">
+                                <center><img src={service.img} alt="" className='h-40 w-auto'/></center>
+                                <h1 className="text-center text-purple-600 font-semibold text-2xl">{service.name}</h1>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div className="">
+                <center>
+                    <a href="">
+                        <div className="mt-8">
+                            <button className='bg-purple-600 text-white py-4 px-16 rounded-full duration-500 hover:bg-purple-700'>Get Started</button>
+                        </div>
+                    </a>
+                </center>
+            </div>
         </div>  
 
         
