f270017a5e221eaca8903cd4f552d4c9391f04ea
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-06 12:36:14 +0530

    Update CurrentCareers.jsx

--- src/components/Careers/CurrentCareers.jsx
+++ src/components/Careers/CurrentCareers.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const CurrentCareers = () => {
+    const Careers = [
+        {
+            id: 1,
+            name: '',
+            exp: [],
+            Responsibilities: '',
+            Requirements: ''
+        },
+    ]
   return (
     <div>CurrentCareers</div>
   )
