7a12c67f11f9fff6ade833baeb7de3ecbc4ed12b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-07 14:28:42 +0530

    Update

--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -7,7 +7,7 @@ const Service = () => {
   return (
     <div className='min-h-screen bg-white md:pt-2 pt-24 px-0'>
         <div className="md:flex justify-between mb-16 md:px-24 px-6 min-h-screen">
-            <div className="w-full md:pt-32 md:mb-0 mb-16">
+            <div className="w-full md:pt-40 md:mb-0 mb-16">
                 <h1 className="text-gray-500 md:text-4xl font-semibold text-xl md:leading-[50px]">Services at <span className='text-purple-800'>BlackAlphaLabs</span></h1>
                 <p className="md:mt-8 mt-4 titleText">
                     At BlackAlphaLabs, we specialize in delivering cutting-edge technology solutions tailored to your unique business needs. Explore our range of services below to see how we can help drive your success.
--- src/pages/Services/ServiceData.jsx
+++ src/pages/Services/ServiceData.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsCloudCheck, BsLaptopFill, BsLink45Deg, BsPersonWorkspace } from 'react-icons/bs'
 
 const ServiceData = () => {
     const OurService = [
@@ -6,68 +7,67 @@ const ServiceData = () => {
             id: 1,
             service: 'Web Development',
             serviceData: 'From single-page applications to complex web platforms, we offer full-stack web development services to bring your ideas to life. Our websites are responsive, user-friendly, and optimized for performance.',
-            technogloy: 'HTML, CSS, JavaScript, React, NodeJS',
-            Logo: ''
+            technology: 'HTML, CSS, JavaScript, React, NodeJS',
+            Logo: <BsLaptopFill className='h-8 w-auto text-white'/>
         },
         {
             id: 2,
             service: 'IT Consulting',
             serviceData: 'Our IT consulting services help you develop a robust technology strategy. We provide insights and guidance to streamline operations, strengthen security, and optimize IT infrastructure.',
-            technogloy: 'Microsoft Azure, AWS, Google Cloud, Cisco, Fortinet, VMware',
-            Logo: ''
+            technology: 'Microsoft Azure, AWS, Google Cloud, Cisco, Fortinet, VMware',
+            Logo: <BsPersonWorkspace className='h-8 w-auto text-white'/>
         },
-
         {
             id: 3,
             service: 'UI/UX Design',
             serviceData: 'We design engaging and intuitive user interfaces that prioritize user satisfaction. Our design process combines creativity with user-centered principles to deliver exceptional digital experiences.',
-            technogloy: 'Adobe XD, Figma, Sketch, InVision, Axure',
-            Logo: ''
+            technology: 'Adobe XD, Figma, Sketch, InVision, Axure',
+            Logo: <BsLaptopFill className='h-8 w-auto text-white'/>
         },
-
         {
             id: 4,
             service: 'IT Support and Maintenance',
             serviceData: 'Maintain a reliable and efficient IT environment with our support and maintenance services. We offer 24/7 technical support and proactive monitoring to ensure minimal downtime.',
-            technogloy: 'Microsoft 365,',
-            Logo: ''
+            technology: 'Microsoft 365',
+            Logo: <BsLaptopFill className='h-8 w-auto text-white'/>
         },
-
-        
         {
             id: 5,
             service: 'API Development and Integration',
             serviceData: 'Our API services facilitate seamless data exchange between systems, improving productivity and operational efficiency. We build robust, secure, and scalable APIs to suit your business needs.',
-            technogloy: 'REST, GraphQL',
-            Logo: ''
+            technology: 'REST, GraphQL',
+            Logo: <BsLink45Deg className='h-8 w-auto text-white'/>
         },
-
         {
             id: 6,
             service: 'Cloud Solutions',
             serviceData: 'Unlock the full potential of cloud computing with our scalable and secure cloud solutions. We help you migrate to and manage cloud environments for enhanced agility and resilience.',
-            technogloy: ' AWS, Microsoft Azure, Google Cloud,',
-            Logo: ''
+            technology: 'AWS, Microsoft Azure, Google Cloud',
+            Logo: <BsCloudCheck className='h-8 w-auto text-white'/>
         },
-    ]
-  return (
-    <div>
+    ];
+
+    return (
         <div className="md:grid grid-cols-3 gap-8">
-            {
-                OurService.map((service, index) => {
-                    return (
-                        <div data-aos="flip-left" className="py-8 px-8 text-center bg-gray-200 mx-4 rounded-xl duration-500 hover:shadow-xl">
-                            <h1 className="font-semibold mb-4 text-purple-600">{service.service}</h1>
-                            <div className="">
-                                {service.serviceData}
-                            </div>
+            {OurService.map((service) => (
+                <div 
+                    key={service.id} 
+                    data-aos="flip-left" 
+                    className="py-8 px-8 text-center bg-gray-200 mx-4 rounded-xl duration-500 hover:shadow-xl"
+                >
+                    <center>
+                        <div className="mt-[-300px] bg-purple-500 px-4 py-4 inline-block mx-auto rounded-full">
+                            {service.Logo}
                         </div>
-                    )
-                })
-            }
+                    </center>
+                    <h1 className="font-semibold mb-4 text-purple-600">{service.service}</h1>
+                    <div>
+                        {service.serviceData}
+                    </div>
+                </div>
+            ))}
         </div>
-    </div>
-  )
+    );
 }
 
-export default ServiceData
+export default ServiceData;
