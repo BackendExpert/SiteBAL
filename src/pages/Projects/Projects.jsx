67484c3029a59d3895b4ce7b4cfd8ea4ebc6acba
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-11 12:06:30 +0530

    Update

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Project from '../../assets/Project.png'
+import BtnDefault from '../../components/Buttons/BtnDefault'
+
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen bg-white md:pt-2 pt-24 px-0'>
+        <div className="md:flex justify-between mb-16 md:px-24 px-6">
+            <div className="w-full md:pt-32 md:mb-0 mb-16">
+                <h1 className="text-gray-500 md:text-4xl font-semibold text-xl md:leading-[50px]">Join the Team at <span className='text-purple-800'>BlackAlphaLabs</span></h1>
+                <p className="md:mt-8 mt-4 titleText">
+                    At BlackAlphaLabs, we are driven by a passion for technology and innovation. We’re always looking for talented, motivated, and creative individuals to join our growing team. If you’re ready to take your career to the next level and be part of a dynamic and forward-thinking company, we want to hear from you!
+                </p>
+
+                <div className="mt-8">
+                    <a href="#CurrentCareer">
+                        <BtnDefault btnTitle={"View Careers"}/>
+                    </a>
+                </div>
+            </div>
+            <div className="w-full md:block hidden mt-20">
+                <img src={Project} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
