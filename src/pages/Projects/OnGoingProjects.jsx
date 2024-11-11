dfe233902f4f6f22afad9bb906bfb02e08822c75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-11 12:24:26 +0530

    Update

---
+++ src/pages/Projects/OnGoingProjects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const OnGoingProjects = () => {
+  return (
+    <div data-aos="zoom-in" className="w-full mx-auto pt-16">
+      <h2 className="text-center text-4xl text-gray-500 font-semibold text-purple-600 mb-12">Our On-Going Projects</h2>
+    
+        <div className="">
+        </div>
+    </div>
+  )
+}
+
+export default OnGoingProjects
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Project from '../../assets/Project.png'
 import BtnDefault from '../../components/Buttons/BtnDefault'
+import OnGoingProjects from './OnGoingProjects'
+import CompletedProjects from './CompletedProjects'
 
 
 const Projects = () => {
@@ -8,14 +10,14 @@ const Projects = () => {
     <div className='min-h-screen bg-white md:pt-2 pt-24 px-0'>
         <div className="md:flex justify-between mb-16 md:px-24 px-6">
             <div className="w-full md:pt-32 md:mb-0 mb-16">
-                <h1 className="text-gray-500 md:text-4xl font-semibold text-xl md:leading-[50px]">Join the Team at <span className='text-purple-800'>BlackAlphaLabs</span></h1>
+                <h1 className="text-gray-500 md:text-4xl font-semibold text-xl md:leading-[50px]">Projects at <br /> <span className='text-purple-800'>BlackAlphaLabs</span></h1>
                 <p className="md:mt-8 mt-4 titleText">
                     At BlackAlphaLabs, we are driven by a passion for technology and innovation. We’re always looking for talented, motivated, and creative individuals to join our growing team. If you’re ready to take your career to the next level and be part of a dynamic and forward-thinking company, we want to hear from you!
                 </p>
 
                 <div className="mt-8">
                     <a href="#CurrentCareer">
-                        <BtnDefault btnTitle={"View Careers"}/>
+                        <BtnDefault btnTitle={"View Projects"}/>
                     </a>
                 </div>
             </div>
@@ -23,6 +25,13 @@ const Projects = () => {
                 <img src={Project} alt="" />
             </div>
         </div>
+
+        <div className="">
+            <OnGoingProjects />
+        </div>
+        {/* <div className="">
+            <CompletedProjects />
+        </div> */}
     </div>
   )
 }
